49731b56c282b7edb07094ea6c1e17ad7a633a12
Elisabeth Ege <piptanto@gmail.com>
2021-10-30 12:00:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+//import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 function App() {
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const url = 'https://geo.ipify.org/api/v2/country?apiKey='+API_KEY+'&ipAddress='
+  
+  const [ipNum, setIp] = useState('not found');
+
+  useEffect(() => loadIp(), []);
+
+  const loadIp = async () => {
+
+    const response = await axios.get(url);
+    console.log(response);
+
+    setIp(response.data);
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>What is my IP-address?</h1>
+      <div className="ipField">
+        <p  style={{textDecorationLine: 'underline'}}>My IP:</p>
+        <p>{ipNum?.ip}</p>
+        <p>{ipNum?.location?.country}</p>
+        <p>{ipNum?.location?.region}</p>
+      </div>
     </div>
   );
 }
